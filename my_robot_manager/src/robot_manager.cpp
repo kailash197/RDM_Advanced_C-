d661bffa04e117658c63a7ffd349575a37d71151
Kailash Khadka <k.khadka343@gmail.com>
2024-09-19 02:55:05 +0000

    Exercise 3.5 Parameterized constructor

--- my_robot_manager/src/robot_manager.cpp
+++ my_robot_manager/src/robot_manager.cpp
@@ -1,16 +1,19 @@
-#include "../include/my_robot_manager/robot_manager.h"
-#include <ros/ros.h>
+#include "my_robot_manager/robot_manager.h"
 
-RobotManager::RobotManager(std::string name, std::string model, float battery,
-                           int hours)
-    : robot_name(name), robot_model(model), battery_capacity_kwh(battery),
-      total_operation_hours(hours) {
+RobotManager::RobotManager(ros::NodeHandle *nh, std::string topic,
+                           std::string name, std::string model)
+    : odom_topic(topic), robot_name(name), robot_model(model) {
+  odom_sub =
+      nh->subscribe(odom_topic, 1000, &RobotManager::odom_callback, this);
   ROS_INFO("Robot initialized.");
 }
-
-void RobotManager::print_specification() {
+void RobotManager::odom_callback(const nav_msgs::Odometry::ConstPtr &msg) {
+  ROS_INFO("%s position (x,y): %lf , %lf", robot_name.c_str(),
+           msg->pose.pose.position.x, msg->pose.pose.position.y);
+}
+void RobotManager::print_specifications() {
   ROS_INFO("Robot Name: %s", robot_name.c_str());
   ROS_INFO("Robot Model: %s", robot_model.c_str());
-  ROS_INFO("Battery Capacity (kWh): %f", battery_capacity_kwh);
+  ROS_INFO("Battery Capacity (kWh): %.2f", battery_capacity_kwh);
   ROS_INFO("Total Operation Hours: %d", total_operation_hours);
 }
--- my_robot_manager/src/robot_manager_node.cpp
+++ my_robot_manager/src/robot_manager_node.cpp
@@ -1,9 +1,12 @@
 #include "my_robot_manager/robot_manager.h"
+#include "ros/node_handle.h"
 #include <ros/ros.h>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "robot_manager");
-  RobotManager robot("my_bot", "TurtleBot3", 0.14, 2400);
-  robot.print_specification();
+  ros::NodeHandle nh;
+  RobotManager robot(&nh, "/robot1/odom", "my_bot", "TurtleBot3");
+  robot.print_specifications();
+  ros::spin();
   return 0;
 }
